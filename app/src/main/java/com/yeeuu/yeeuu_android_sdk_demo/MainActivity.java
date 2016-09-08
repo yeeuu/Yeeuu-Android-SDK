41bce3cd974a75c9d6db07cc17875a4d44b3eeed
Terry <Terry@TerrydeMBP.lan>
2016-09-08 15:05:11 +0800

    fix some bugs

--- app/src/main/java/com/yeeuu/yeeuu_android_sdk_demo/MainActivity.java
+++ app/src/main/java/com/yeeuu/yeeuu_android_sdk_demo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.yeeuu.yeeuu_android_sdk_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,7 +19,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    EditText tokenet;
     ListView locklv;
     CommonAdapter<Lock> adapter;
 
@@ -25,20 +27,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Yeeuu.init("6b30f1c04d070e4541f65b10d06c17b7930a8279", "559c8867766d315ce8000000");
-
+        tokenet = (EditText) findViewById(R.id.edit_token);
         findViewById(R.id.getlock).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String token = tokenet.getText().toString();
+                Yeeuu.init(token, null);
                 Yeeuu.getLockList(new LockLisener() {
                     @Override
                     public void onSuccess(List<Lock> lockList) {
-                        setlocklist(lockList);
+                        Toast.makeText(MainActivity.this, "获取成功", Toast.LENGTH_SHORT).show();
+                        if (lockList.isEmpty())
+                            Toast.makeText(MainActivity.this, "该用户无门锁", Toast.LENGTH_SHORT).show();
+                        else
+                            setlocklist(lockList);
                     }
 
                     @Override
                     public void onFailed(int errCode, String errMsg) {
-
+                        Toast.makeText(MainActivity.this, "获取门锁列表失败", Toast.LENGTH_SHORT).show();
+                        Log.e("获取门锁列表失败  " + errCode, errMsg);
                     }
                 });
             }
@@ -50,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         adapter = new CommonAdapter<Lock>(this, R.layout.lock_item, lockList) {
             @Override
             protected void convert(ViewHolder viewHolder, final Lock lock, int position) {
-                viewHolder.setText(R.id.lockid, "门锁ID: " + lock.getId());
+                viewHolder.setText(R.id.hotel, "酒店名: " + lock.getHotel());
                 viewHolder.setText(R.id.room, "房间号: " + lock.getRoom());
                 viewHolder.getView(R.id.open).setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -58,14 +66,13 @@ public class MainActivity extends AppCompatActivity {
                         Yeeuu.OpenLock(lock.getId(), new OpenLisener() {
                             @Override
                             public void onSucc() {
-                                Toast.makeText(MainActivity.this, "开门成功"
-                                        , Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "开门成功", Toast.LENGTH_SHORT).show();
                             }
 
                             @Override
                             public void onFail(int errCode, String errMsg) {
-                                Toast.makeText(MainActivity.this, "开门失败: " + errMsg
-                                        , Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, "开门失败", Toast.LENGTH_SHORT).show();
+                                Log.e("开门失败  " + errCode, errMsg);
                             }
                         });
                     }
